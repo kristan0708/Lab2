9fc5baae94e882a40aa734a322f98b0a36911b6d
kristan0708 <adriosulakza.25@ichat.sp.edu.sg>
2026-04-30 15:05:31 +0800

    Finished!!

--- Lab2.py
+++ Lab2.py
@@ -1,7 +1,31 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
 
+"""
 def funcName(parameter1, parameter2):
     print("this is a dummy function")
     return 10
 
-print(funcName(1,1))
+
+
+def display_main_menu():
+    print("Enter some numbers seperated by commas (e.g. 5, 67, 32)")
+
+def get_user_input():
+    user_input = input(":")
+    string_list = user_input.split(",")
+    float_list = []
+
+    for num_str in string_list:
+        float_list.append(float(num_str))
+    return float_list
+
+def calc_average(list_float_num):
+    avg = sum(list_float_num) / len(list_float_num)
+    return avg
+
+def find_min_max(list_float_num):
+    min_max = [min(list_float_num),max(list_float_num)]
+"""
+
+abc = [1,2,3,4,5]
+print(min(abc))
