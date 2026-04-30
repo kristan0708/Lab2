0717b71b46b725071cad5d4614c1dccb37f2e649
kristan0708 <adriosulakza.25@ichat.sp.edu.sg>
2026-04-30 15:45:03 +0800

    New Version v1.2

--- Lab2.py
+++ Lab2.py
@@ -5,6 +5,14 @@ def funcName(parameter1, parameter2):
     print("this is a dummy function")
     return 10
 
+"""
+
+def main():
+    display_main_menu()
+    float_list = get_user_input()
+    sort_temp = sort_temperature(float_list)
+    median = float(calc_median_temperature(sort_temp))
+    print(median)
 
 
 def display_main_menu():
@@ -20,12 +28,26 @@ def get_user_input():
     return float_list
 
 def calc_average(list_float_num):
-    avg = sum(list_float_num) / len(list_float_num)
+    avg = float(sum(list_float_num) / len(list_float_num))
     return avg
 
-def find_min_max(list_float_num):
-    min_max = [min(list_float_num),max(list_float_num)]
-"""
+def sort_temperature(temp_list):
+    sorted_temp = sorted(temp_list)
+    return sorted_temp
 
-abc = [1,2,3,4,5]
-print(min(abc))
+def find_min_max(list_float_num):
+    min_max = [int(min(list_float_num)),int(max(list_float_num))]
+    return min_max
+
+def calc_median_temperature(median_temp):
+    if len(median_temp)%2 == 0:
+        sum_median = float(median_temp[(len(median_temp)//2)] + median_temp[(len(median_temp)//2 - 1)])
+        median = sum_median/2
+    else:
+        index = round(len(median_temp)//2)
+        median = float(median_temp[index])
+
+    return median
+
+if __name__ == '__main__':
+    main()
